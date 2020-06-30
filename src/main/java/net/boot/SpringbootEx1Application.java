95c5849fff83b4fb2d7efd7c555dc687fe57644e
ysj13 <ysj13@kakao.com>
2020-07-01 03:16:52 +0900

    first commit

---
+++ src/main/java/net/boot/SpringbootEx1Application.java
@@ -0,0 +1,13 @@
+package net.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/boot/SpringbootEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
