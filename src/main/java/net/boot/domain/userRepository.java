8d328c20a0822d546c7ef827ad0980aabf9dd186
ysj13 <ysj13@kakao.com>
2020-07-02 16:57:38 +0900

    jpa, h2 db 적용

---
+++ src/main/java/net/boot/domain/userRepository.java
@@ -0,0 +1,7 @@
+package net.boot.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//extends JpaRepository<어떤클래스인지?, id의 타입>
+public interface userRepository extends JpaRepository<User, Long> {
+
+}
--- src/main/java/net/boot/web/UserController.java
+++ src/main/java/net/boot/web/UserController.java
@@ -3,26 +3,34 @@ package net.boot.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.boot.domain.User;
+import net.boot.domain.userRepository;
+
 @Controller
 public class UserController {
 	
-	private List<User> users = new ArrayList<>();
+//	private List<User> users = new ArrayList<>();
+	
+	@Autowired
+	private userRepository userRepository;
 	
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println("user : " + user);
-		users.add(user);
+//		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 
